101a8097016d9da753620b714ed1959a0262dc04
Sam Mina <samandmissy2010@gmail.com>
2024-11-30 16:13:22 -0500

    App.js - page routing management

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
+
+// Routes between pages.
+
+const App = () => {
+  return (
+   
+    <Router>
+     
+      <Header />
+
+      <main>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<About />} /> {/* Default route */}
+        </Routes> 
+      </main>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
+
