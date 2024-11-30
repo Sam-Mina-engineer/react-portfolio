b6989fafbab69bece95da5b79dea3d82011db8d4
Sam Mina <samandmissy2010@gmail.com>
2024-11-29 21:00:33 -0500

    Header.js - display portfolio title nad integrate navigation bar

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+// Import React
+
+import React from 'react';
+import Navigation from './Navigation';
+
+// Header Component
+// This component will be used across multiple pages.
+
+const Header = () => {
+  return (
+
+    <header>
+      {/* Site title: Displays the developer's name */}
+
+      <h1>Sam Mina's Portfolio</h1>
+
+      {/* Navigation component: Handles the navigation links (About, Portfolio, Contact, Resume) */}
+
+      <Navigation />
+    </header>
+  );
+};
+
+// Exporting the Header component so that it can be used elsewhere
+
+export default Header;
+
