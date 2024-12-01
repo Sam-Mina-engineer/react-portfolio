98542138a110a53d0cd6ba2a049b34e3254d747d
Sam Mina <samandmissy2010@gmail.com>
2024-11-30 20:06:52 -0500

    removed alias from vite

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-// Export configuration for Vite server
-
 export default defineConfig({
-  plugins: [react()], // Adding React plugin to support JSX and React features
+  plugins: [react()],
   server: {
     port: 3001, 
-    open: true, // Automatically open app
+    open: true,
   },
   build: {
-    outDir: 'dist', // Output directory for the build files
-    emptyOutDir: true, // Empty output directory first
-  },
-  resolve: {
-    alias: {
-      '@': '/src', // '@' to refer to 'src'
-    },
+    outDir: 'dist',
+    emptyOutDir: true,
   },
 });
