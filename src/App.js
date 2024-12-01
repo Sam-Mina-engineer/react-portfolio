5278e82be373c4e3f5afa9886600e2d70e812779
Sam Mina <samandmissy2010@gmail.com>
2024-11-30 20:38:30 -0500

    Fixed case sensitivity issues

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header'; // Corrected import to include Header component
 import Footer from './components/Footer';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
@@ -10,25 +11,23 @@ import Contact from './pages/Contact';
 
 const App = () => {
   return (
-   
     <Router>
-     
       <Header />
 
       <main>
         <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/contact" element={<Contact />} />
           <Route path="/" element={<About />} /> {/* Default route */}
-          <Route path="*" element={<About />} />
-        </Routes> 
+          <Route path="/About" element={<About />} />
+          <Route path="/Portfolio" element={<Portfolio />} />
+          <Route path="/Resume" element={<Resume />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="*" element={<About />} /> {/* Fallback route */}
+        </Routes>
       </main>
+
       <Footer />
     </Router>
   );
 };
 
 export default App;
-
